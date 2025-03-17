786ba83d1108e45484070fc6bd258d558cc50f78
Richard Le <169632088+rlechuong@users.noreply.github.com>
2025-03-17 18:42:07 -0400

    Add initial files and basic mathematical functions

---
+++ scripts.js
@@ -0,0 +1,34 @@
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    if (operator === "+") {
+        return add(a, b);
+    }
+    else if (operator === "-") {
+        return subtract(a, b);
+    }
+    else if (operator === "*") {
+        return multiply(a, b);
+    }
+    else if (operator === "/") {
+        return divide(a, b);
+    }
+}
